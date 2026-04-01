98c20ea08e66e6542087fdc55b21b47f5c1b73d1
WujudAhmed <s202279120@kfupm.edu.sa>
2026-04-01 11:39:26 +0300

    parser.js is done

--- utils/parser.js
+++ utils/parser.js
@@ -6,6 +6,6 @@ export function parseNumbers(input) {
 }
 
 export function isValidOperation(operation) {
-  const validOps = ["add", "subtract", "multiply", "divide"];
+  const validOps = [" add ", " subtract ", " multiply ", " divide "];
   return _.includes(validOps, operation);
 }
